5236ea950f638d0060080b4b48933d8026d81119
Shubham Pawar <112054009+ShubhamPawar-3333@users.noreply.github.com>
2022-11-09 09:33:17 +0530

    Update Detection_in_image.py

--- Detection_in_image.py
+++ Detection_in_image.py
@@ -5,20 +5,20 @@ face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
 
 # Reading the image
-img = cv.imread('test.jpg')
+image = cv.imread('test.jpg')
 
 # Converting into grayscale
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 # Face detection
 faces = face_cascade.detectMultiScale(gray, 1.2, 6)
 
 # Marking the face region
 for (x, y, w, h) in faces:
-    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
     # Reading the Region of Interest and converting to greyscale
     roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
+    roi_color = image[y:y+h, x:x+w]
     # Detecting the eyes
     eyes = eye_cascade.detectMultiScale(roi_gray)
     # Marking the eyes
@@ -26,5 +26,5 @@ for (x, y, w, h) in faces:
         cv.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 0, 255), 2)
         
 # Displaying the outcome
-cv.imshow('img', img)
+cv.imshow('Detection Outcome', image)
 cv.waitKey()
